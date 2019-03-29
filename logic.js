872020fac15e54f786ca22af4efc26a15d348232
sponre01 <sponre01@gmail.com>
2019-03-28 20:40:52 -0500

    level one complete

--- logic.js
+++ logic.js
@@ -1,11 +1,21 @@
-// Function to determine marker size based on population
+// Function to determine marker size based on magnitude
 function markerSize(magnitude) {
   return magnitude * 30000;
 }
 
+// Function to determine marker color based on magnitude
+function getColor(magnitude) {
+  return magnitude > 5 ? '#800026' :
+          magnitude > 4  ? '#BD0026' :
+          magnitude > 3  ? '#E31A1C' :
+          magnitude > 2  ? '#FC4E2A' :
+          magnitude > 1   ? '#FD8D3C' :
+                    '#FFEDA0';
+}
+
 // Creating map object
 var myMap = L.map("map", {
-  center: [38.0902, -115.7129],
+  center: [38.0902, -95.7129],
   zoom: 5
 });
 
@@ -24,8 +34,8 @@ d3.json(link, function(response) {
 
   console.log(response.features);
   data = response.features
-  console.log(data[0].properties.mag)
 
+  // add circles and correspond them to mag size
   var markerArray = [];
 
   for (var i = 0; i < data.length; i++) {
@@ -38,160 +48,76 @@ d3.json(link, function(response) {
           stroke: false,
           fillOpacity: 0.75,
           color: "white",
-          fillColor: "white",
+          fillColor: getColor(data[i].properties.mag),
           radius: markerSize(data[i].properties.mag)
         }));
     }
   };
   L.layerGroup(markerArray).addTo(myMap);
+
+  // make legend
+  var legend = L.control({position: 'bottomright'});
+  legend.onAdd = function (map) {
+  
+      var div = L.DomUtil.create('div', 'info legend'),
+          grades = [0, 1, 2, 3, 4, 5],
+          labels = [];
+  
+      // loop through our magnitude intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style=\"background:' + getColor(grades[i] + 1) + '\"></li> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      }
+  
+      return div;
+  };
+  
+  legend.addTo(myMap);
+
+  // var legend = L.control({position: 'bottomright'});
+  // legend.onAdd = function (map) {
+
+  // var div = L.DomUtil.create('div', 'info legend');
+  // labels = ['<strong>Categories</strong>'],
+  // categories = ['Road Surface','Signage','Line Markings','Roadside Hazards','Other'];
+
+  // for (var i = 0; i < categories.length; i++) {
+
+  //         div.innerHTML += 
+  //         labels.push(
+  //             '<i class="circle" style="background:' + getColor(categories[i]) + '"></i> ' +
+  //         (categories[i] ? categories[i] : '+'));
+
+  //     }
+  //     div.innerHTML = labels.join('<br>');
+  // return div;
+  // };
+  // legend.addTo(myMap);
+
+// // Set up the legend
+// var legend = L.control({ position: "bottomright" });
+// legend.onAdd = function() {
+//   var div = L.DomUtil.create("div", "info legend");
+//   var limits = geojson.options.limits;
+//   var colors = geojson.options.colors;
+//   var labels = [];
+
+//   // Add min & max
+//   var legendInfo = "<h1>Median Income</h1>" +
+//     "<div class=\"labels\">" +
+//       "<div class=\"min\">" + limits[0] + "</div>" +
+//       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+//     "</div>";
+
+//   div.innerHTML = legendInfo;
+
+  // limits.forEach(function(limit, index) {
+  //   labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  // });
+
+//   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+//   return div;
+// };
+
 });
-
-
-
-
-
-
-
-
-
-
-
-// // Grabbing our GeoJSON data..
-// d3.json(link, function(data) {
-//   L.geoJson(data).addTo(map);
-// });
-
-
-
-
-
-
-
-
-
-// var geojson;
-
-// // Grab data with d3
-// d3.json(link, function(data) {
-//   geojson = L.choropleth(data, {
-//     valueProperty: "MHI",
-//     scale: ["#008000", "#ff0000"],
-//     steps: 6,
-//     mode: "q",
-//     style: {
-//       color: "#fff",
-//       weight: 1,
-//       fillOpacity: 0.8
-//     },
-
-//   }).addTo(myMap);
-
-//   // Set up the legend
-//   var legend = L.control({ position: "bottomright" });
-//   legend.onAdd = function() {
-//     var div = L.DomUtil.create("div", "info legend");
-//     var limits = geojson.options.limits;
-//     var colors = geojson.options.colors;
-//     var labels = [];
-
-//     // Add min & max
-//     var legendInfo = "<h1>Median Income</h1>" +
-//       "<div class=\"labels\">" +
-//         "<div class=\"min\">" + limits[0] + "</div>" +
-//         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-//       "</div>";
-
-//     div.innerHTML = legendInfo;
-
-//     limits.forEach(function(limit, index) {
-//       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-//     });
-
-//     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-//     return div;
-//   };
-
-//   // Adding legend to the map
-//   legend.addTo(myMap);
-
-// });
-
-
-
-
-
-
-
-
-// // Define arrays to hold created city and state markers
-// var cityMarkers = [];
-// var stateMarkers = [];
-
-// // Loop through locations and create city and state markers
-// for (var i = 0; i < locations.length; i++) {
-//   // Setting the marker radius for the state by passing population into the markerSize function
-//   stateMarkers.push(
-//     L.circle(locations[i].coordinates, {
-//       stroke: false,
-//       fillOpacity: 0.75,
-//       color: "white",
-//       fillColor: "white",
-//       radius: markerSize(locations[i].state.population)
-//     })
-//   );
-
-//   // Setting the marker radius for the city by passing population into the markerSize function
-//   cityMarkers.push(
-//     L.circle(locations[i].coordinates, {
-//       stroke: false,
-//       fillOpacity: 0.75,
-//       color: "purple",
-//       fillColor: "purple",
-//       radius: markerSize(locations[i].city.population)
-//     })
-//   );
-// }
-
-// // Define variables for our base layers
-// var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "mapbox.streets",
-//   accessToken: API_KEY
-// });
-
-// var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "mapbox.dark",
-//   accessToken: API_KEY
-// });
-
-// // Create two separate layer groups: one for cities and one for states
-// var states = L.layerGroup(stateMarkers);
-// var cities = L.layerGroup(cityMarkers);
-
-// // Create a baseMaps object
-// var baseMaps = {
-//   "Street Map": streetmap,
-//   "Dark Map": darkmap
-// };
-
-// // Create an overlay object
-// var overlayMaps = {
-//   "State Population": states,
-//   "City Population": cities
-// };
-
-// // Define a map object
-// var myMap = L.map("map", {
-//   center: [37.09, -95.71],
-//   zoom: 5,
-//   layers: [streetmap, states, cities]
-// });
-
-// // Pass our map layers into our layer control
-// // Add the layer control to the map
-// L.control.layers(baseMaps, overlayMaps, {
-//   collapsed: false
-// }).addTo(myMap);
